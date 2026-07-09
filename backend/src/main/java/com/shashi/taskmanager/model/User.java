84de7f94d2d78fd4955ae2f45df3961aa64f94e2
Shashidhar Reddy <shashirdy74@gmail.com>
2026-07-09 09:51:43 -0400

    Create User.java

---
+++ backend/src/main/java/com/shashi/taskmanager/model/User.java
@@ -0,0 +1,46 @@
+package com.shashi.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private Role role = Role.USER;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();
+
+    @OneToMany(mappedBy = "assignee", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private Set<Task> tasks = new HashSet<>();
+}
